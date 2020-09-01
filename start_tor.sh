da2a8084a88fb254328924b71f55ccd75d5d7817
Charles Montgomery <charles.montgomery@charter.net>
2020-09-01 19:03:54 +0000

    private key management in tor hidden services has changed\!

--- start_tor.sh
+++ start_tor.sh
@@ -12,16 +12,20 @@ then
 		done
 	
 		cp /hidsrv/hostname ${TOR_CONFIG_DIR}/hostname
-		cp /hidsrv/private_key ${TOR_CONFIG_DIR}/private_key		
+		cp /hidsrv/hs_ed25519_public_key ${TOR_CONFIG_DIR}/hs_ed25519_public_key
+		cp /hidsrv/hs_ed25519_secret_key ${TOR_CONFIG_DIR}/hs_ed25519_secret_key
 	else
 		if [[ ! -e /hidsrv/hostname ]]
 		then
 			cp ${TOR_CONFIG_DIR}/hostname /hidsrv/hostname
-			cp ${TOR_CONFIG_DIR}/private_key /hidsrv/private_key
+			cp ${TOR_CONFIG_DIR}/hs_ed25519_public_key /hidsrv/hs_ed25519_public_key
+			cp ${TOR_CONFIG_DIR}/hs_ed25519_secret_key /hidsrv/hs_ed25519_secret_key
 			chown debian-tor:debian-tor /hidsrv/hostname
 			chmod 600 /hidsrv/hostname
-			chown debian-tor:debian-tor /hidsrv/private_key
-			chmod 600 /hidsrv/private_key
+			chown debian-tor:debian-tor /hidsrv/hs_ed25519_public_key
+			chmod 600 /hidsrv/hs_ed25519_public_key
+			chown debian-tor:debian-tor /hidsrv/hs_ed25519_secret_key
+			chmod 600 /hidsrv/hs_ed25519_secret_key
 		fi
 		service ssh start
 		service tor start
